caf2bf2ab783b41fd2524db77fb8144c9c871551
dgupta13 <deepika.gupta9@gmail.com>
2021-08-05 23:19:20 +1000

    intial unit testing setup

---
+++ src/App.spec.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+let wrapped = shallow(<App />);
+
+describe("App", () => {
+  it("should render the App Component correctly", () => {
+    expect(wrapped).toMatchSnapshot();
+  });
+});
